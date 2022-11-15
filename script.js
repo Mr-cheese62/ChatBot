d549c564f0313f1531e2b355a8007a607dd941b0
Riley Paffrath <mr.cheese679@gmail.com>
2022-11-14 18:38:02 -0800

    second commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var nameGuy = prompt("what is your name?");
+var age = prompt("hello " + nameGuy + " how old are you?");
+if (age > 18){
+    alert("your kinda old " + nameGuy + " ngl")
+}else{
+    alert("you are a young person i like it your very cool B)")
+     var young = prompt("but you have to prove that you are young so what is 9 + 10 this math test will tell me if you are young or your old")
+    if (young === "21"){
+         var secretCode = prompt("you have passed the test congrats!")
+        if (secretCode === "misaMisa123"){
+            {window.location = "https://youtu.be/xvFZjo5PgG0"}
+        }if(secretCode === "overRide"){
+            window.location = "index3.0.html"
+        }else{
+            alert("you are soooooooooooooooooooooooooooooooo epik B)")
+        }
+    }else{
+         alert("HA I KNEW IT YOU ARE A LIAR AND A SCOUNDREAL! YOU SHOULD BE ASHAMED FOR LIEING ABOUT YOUR AGE")
+    }
+}
